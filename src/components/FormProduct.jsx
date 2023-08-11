f86dd9a482643bcf2241444e0746d1f2a4181306
LJR411~ <ljremulla@yahoo.com>
2023-08-11 14:48:27 -0700

    init commit

---
+++ src/components/FormProduct.jsx
@@ -0,0 +1,47 @@
+import React,{useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+const FormProduct = () => {
+    const navigate = useNavigate()
+    const [title, setTitle] = useState()
+    const [price, setPrice] = useState()
+    const [description, setDescription] = useState()
+    
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+        axios.post('http://localhost:8000/api/product/create',{title,price,description})
+            .then(response=>{
+                console.log(response)
+                navigate('/')
+            })
+            .catch(err=>console.log(err))
+    }
+    return (
+        <div>
+            <h1>Create a Product</h1>
+            <form onSubmit={handleSubmit}>
+                <div style={{ flex: 3, flexDirection: 'column' }}>
+                    <div style={{ marginBottom: 10 }}>
+                        <label htmlFor="title" style={{ marginRight: 5 }}>Title</label>
+                        <input type="text" name="title" onChange={e=>setTitle(e.target.value)}/>
+                    </div>
+                    <div style={{ marginBottom: 10 }}>
+                        <label htmlFor="price" style={{ marginRight: 5 }} >Price</label>
+                        <input type="number" name='price'onChange={e=>setPrice(e.target.value)} />
+                    </div>
+                    <div style={{ marginBottom: 10 }}>
+                        <label htmlFor="description" style={{ marginRight: 5 }}>Description</label>
+                        <textarea name="description" id="" cols="20" rows="5" onChange={e=>setDescription(e.target.value)}></textarea>
+                    </div>
+                    <button type='submit'>Create</button>
+                </div>
+            </form>
+
+
+
+        </div>
+    )
+}
+
+export default FormProduct
